1df910524e1b99624c373a27ff8c76153cc63519
songintae <songintae92@hanmail.net>
2022-03-07 11:56:18 +0900

    zMartStreamApplication 로직 추가 및 Model 객체들 추가

---
+++ src/main/java/com/example/kafkastreamsinaction/chapter03/ZMartStreamsApplicationConfiguration.java
@@ -0,0 +1,42 @@
+package com.example.kafkastreamsinaction.chapter03;
+
+import com.example.kafkastreamsinaction.model.Purchase;
+import com.example.kafkastreamsinaction.model.PurchasePattern;
+import com.example.kafkastreamsinaction.model.RewardAccumulator;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.serializer.JsonSerde;
+
+import java.util.function.Consumer;
+
+@Configuration
+public class ZMartStreamsApplicationConfiguration {
+
+    private static final Serde<String> stringSerde = Serdes.String();
+    private static final Serde<Purchase> purchaseSerde = new JsonSerde<>(Purchase.class);
+    private static final Serde<PurchasePattern> purchasePatternSerde = new JsonSerde<>(PurchasePattern.class);
+    private static final Serde<RewardAccumulator> rewardAccumulatorSerde = new JsonSerde<>(RewardAccumulator.class);
+
+    @Bean
+    public Consumer<KStream<String, Purchase>> zMartStreamApplication() {
+        return input -> {
+            final KStream<String, Purchase> purchaseKStream = input
+                    .mapValues(p -> Purchase.builder(p).maskCreditCard().build());
+
+            purchaseKStream
+                    .mapValues(purchase -> PurchasePattern.builder(purchase).build())
+                    .to("patterns", Produced.with(stringSerde, purchasePatternSerde));
+
+            purchaseKStream
+                    .mapValues(purchase -> RewardAccumulator.builder(purchase).build())
+                    .to("rewards", Produced.with(stringSerde, rewardAccumulatorSerde));
+
+            purchaseKStream.to("purchases", Produced.with(stringSerde, purchaseSerde));
+        };
+    }
+
+}
